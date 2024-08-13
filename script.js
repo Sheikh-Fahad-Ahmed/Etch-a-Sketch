d123c80b5385b81b16ec0f0d1f703b0fd8e55bac
Fahad Ahmed <fahad838@gmail.com>
2024-08-13 18:21:08 +0530

    Create 16x16 squares with red border
    First, created a square having width and height
    of 10px with a red border then used a for loop
    to create 16 more squares and 16 rows creating a
    16x16 grid.
    
    Created the grid by leveraging for loop. Inside
    loop i created each row divs and another for loop
    for square divs. Used flexbox to align them
    in a grid pattern.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let div = document.querySelector(".container");
 
-let row = document.createElement("div");
-let square = document.createElement("div");
-row.classList.add("row");
-square.classList.add("square");
 
-for (let i = 0 ; i < 16 ; i++) {
-    
+
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.className = `row row${i+1}`;
+    div.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+        let square = document.createElement("div");
+        square.className = `square square${j+1}`;
+        row.appendChild(square);
+    }
 }
