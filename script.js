56efadf2a6cd3bbc00d5c72fd45123aff49328f7
Fahad Ahmed <fahad838@gmail.com>
2024-08-13 19:15:29 +0530

    Add a hover effect to the squares
    Added an effect to the squares where if you mouse
    over them they change to a different color and
    persist like that making it possible to draw a
    sketch.
    
    Achieved this by leveraging event listener. First,
    i created a variable boxes that has a node list of
    all the squares. Then used for each on that
    variable and implemented event listeners on each
    box.

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ let div = document.querySelector(".container");
 
 
 for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.className = `row row${i+1}`;
+    var row = document.createElement("div");
+    row.className = `row row${i + 1}`;
     div.appendChild(row);
     for (let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.className = `square square${j+1}`;
+        var square = document.createElement("div");
+        square.className = `square square${j + 1}`;
         row.appendChild(square);
     }
 }
+
+let boxes = document.querySelectorAll(".square");
+
+[...boxes].forEach(box => {
+    box.addEventListener("mouseover", () => {
+        box.classList.add("hover");
+    });
+});
+
