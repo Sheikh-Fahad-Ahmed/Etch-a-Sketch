7eb67faf70b40c8e888767270ae1de7785834058
Fahad Ahmed <fahad838@gmail.com>
2024-08-13 17:55:53 +0530

    Create variables to begin constructing squares.
    Created a div variable called row and a div
    variable called square. Added class name row and
    square respectively. Created another div variable
    called div. This is the parent tag.

---
+++ script.js
@@ -0,0 +1,10 @@
+let div = document.querySelector(".container");
+
+let row = document.createElement("div");
+let square = document.createElement("div");
+row.classList.add("row");
+square.classList.add("square");
+
+for (let i = 0 ; i < 16 ; i++) {
+    
+}
