7949ecce0c1fd819fd9d398318333774296f9e34
Fahad Ahmed <fahad838@gmail.com>
2024-08-14 18:27:13 +0530

    Add reset button and is cleaner and readable
    Refactored some of the code to be inside a
    function. Now Most of the code is in functions.
    startGrid is the beginning of the program and
    hover is for the hovering effect of squares.
    Rather than creating grid again when user chooses
    an input, startGrid is called again with the user
    choice as argument. deleteGrid is the same.
    
    Added a RESET button. It reset the current grid
    with the same size.

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 let div = document.querySelector(".container");
 let btn = document.querySelector(".pick");
+let reset = document.querySelector(".reset");
 let tilesPerSide = 16;
+startGrid(tilesPerSide);
+
+
+function startGrid(tilesPerSide) {
+    for (let i = 0; i < tilesPerSide; i++) {
+        let row = document.createElement("div");
+        row.className = `row row${i + 1}`;
+        div.appendChild(row);
+        for (let j = 0; j < tilesPerSide; j++) {
+            let square = document.createElement("div");
+            square.className = `square square${j + 1}`;
+            square.setAttribute("style", `border: 1px solid #FBE094; width:${700 / tilesPerSide}px; height:${700 / tilesPerSide}px; padding: 0;`)
+            row.appendChild(square);
+        }
+    }
+    hover();
+}
+
+function hover() {
+    let boxes = document.querySelectorAll(".square");
+    [...boxes].forEach(box => {
+        box.addEventListener("mouseover", () => {
+            box.classList.add("hover");
+        });
+    });
+}
+
+
+function deleteGrid() {
+    let rows = document.querySelectorAll(".row");
+    [...rows].forEach(row => {
+        row.remove();
+    });
+}
+
 
 btn.addEventListener('click', () => {
     deleteGrid();
@@ -10,56 +46,10 @@ btn.addEventListener('click', () => {
             break;
         }
     }
-    createGrid(tilesPerSide);
+    startGrid(tilesPerSide);
 })
 
-
-for (let i = 0; i < tilesPerSide; i++) {
-    let row = document.createElement("div");
-    row.className = `row row${i + 1}`;
-    div.appendChild(row);
-    for (let j = 0; j < tilesPerSide; j++) {
-        let square = document.createElement("div");
-        square.className = `square square${j + 1}`;
-        square.setAttribute("style", `border: 1px solid #FBE094; width:${700 / tilesPerSide}px; height:${700 / tilesPerSide}px; padding: 0;`)
-        row.appendChild(square);
-    }
-}
-
-
-let boxes = document.querySelectorAll(".square");
-[...boxes].forEach(box => {
-    box.addEventListener("mouseover", () => {
-        box.classList.add("hover");
-    });
-});
-
-
-function createGrid(tilesPerSide) {
-    for (let i = 0; i < tilesPerSide; i++) {
-        var row = document.createElement("div");
-        row.className = `row row${i + 1}`;
-        div.appendChild(row);
-        for (let j = 0; j < tilesPerSide; j++) {
-            var square = document.createElement("div");
-            square.className = `square square${j + 1}`;
-            square.setAttribute("style", `border: 2px solid #FBE094; width:${700 / tilesPerSide}px; height:${700 / tilesPerSide}px; padding: 20px; padding:0;`)
-            row.appendChild(square);
-        }
-    }
-    let boxes = document.querySelectorAll(".square");
-
-    [...boxes].forEach(box => {
-        box.addEventListener("mouseover", () => {
-            box.classList.add("hover");
-        });
-    });
-}
-
-function deleteGrid() {
-    let rows = document.querySelectorAll(".row");
-    [...rows].forEach(row => {
-        row.remove();
-    });
-}
-
+reset.addEventListener('click', () => {
+    deleteGrid();
+    startGrid(tilesPerSide);
+})
