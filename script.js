d14befae9926879834d328221ecc5965c60ffe28
Fahad Ahmed <fahad838@gmail.com>
2024-08-14 11:47:06 +0530

    Add: logic to button; Refactor: create, del grid
    Added logic to button, it nows shows a prompt to
    get user input for number of squares per side.
    
    Also refactored code that creates grid into a
    function called createGrid.
    
    Created a function called deleteGrid, this is to
    ensure that whenever user inputs a value, the
    current grid gets deleted and a new grid is
    created. Otherwise they would just add to the
    current grid.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let div = document.querySelector(".container");
 
+let btn = document.querySelector(".pick");
+let tilesPerSide = 16;
+
+btn.addEventListener('click', () => {
+    deleteGrid();
+    while (true) {
+        tilesPerSide = prompt("Choose number of tiles per side...(max: 100, min: 16)", " 16");
+        if ((tilesPerSide <= 100) && (tilesPerSide >= 16)) {
+            break;
+        }
+    }
+    createGrid(tilesPerSide);
+})
 
 
-
-for (let i = 0; i < 16; i++) {
-    var row = document.createElement("div");
+for (let i = 0; i < tilesPerSide; i++) {
+    let row = document.createElement("div");
     row.className = `row row${i + 1}`;
     div.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        var square = document.createElement("div");
+    for (let j = 0; j < tilesPerSide; j++) {
+        let square = document.createElement("div");
         square.className = `square square${j + 1}`;
         row.appendChild(square);
     }
@@ -22,3 +34,23 @@ let boxes = document.querySelectorAll(".square");
     });
 });
 
+
+function createGrid(tilesPerSide) {
+    for (let i = 0; i < tilesPerSide; i++) {
+        var row = document.createElement("div");
+        row.className = `row row${i + 1}`;
+        div.appendChild(row);
+        for (let j = 0; j < tilesPerSide; j++) {
+            var square = document.createElement("div");
+            square.className = `square square${j + 1}`;
+            row.appendChild(square);
+        }
+    }
+}
+
+function deleteGrid() {
+    let rows = document.querySelectorAll(".row");
+    [...rows].forEach(row => {
+        row.remove();
+    });
+}
